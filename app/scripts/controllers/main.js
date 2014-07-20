7291b347cb17545725eea0268d15b596616a696b
Aveline Estié <hello@aveline.be>
2014-07-20 13:24:50 +0200

    one recipe

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,24 @@ angular.module('alKhemiApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.recipes = [
+        {
+            "id":"001",
+            "name":"Sandwich",
+            "use":"Restores 100 HP",
+            "items":[
+            {
+                "name":"White Bread",
+                "num":"1"
+            },
+            {
+                "name":"Tender Beef",
+                "num":"1"
+            }
+            ]
+        }
+    ];
+
+    console.log($scope.recipes);
   });
